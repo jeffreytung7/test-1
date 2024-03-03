95b97fb783e08f81d130fa1eaf435a06499e9375
jeffreytung7 <81663048+jeffreytung7@users.noreply.github.com>
2024-03-02 16:34:36 -0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const { data, error } = await useAsyncData('test', async () => {
+    if (process.server) {
+        console.log('Server: hi');
+    } else if (process.client) {
+        console.log('Client: hi');
+    }
+    const response = await $fetch('/api/test')
+});
+</script>
+
+<template>
+    <main>
+        hi
+    </main>
+</template>
